baff5131a6fc612b84ee8d49453328bfec25ce5f
Vova Babushkin <babushkinvova05@gmail.com>
2025-05-11 09:32:13 +0700

    init api gateway

---
+++ kr2/api-gateway/internal/config/config.go
@@ -0,0 +1,13 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env  string     `yaml:"env" env-default:"local"`
+	GRPC GRPCConfig `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
